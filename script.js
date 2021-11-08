cb4173622c21d081884aff3d382dc1d15be18417
HenockAdane <adanehenock@gmail.com>
2021-11-08 01:57:47 +0000

    updated code and added a route container

--- script.js
+++ script.js
@@ -1,16 +1,64 @@
-let menuBtn = document.querySelector("#phone-menu");
-let navBar = document.querySelector("nav");
+let menuBtn = document.querySelector(".menu-btn");
+let navBar = document.querySelector("#phone-nav");
+let body = document.querySelector("body")
+let navLinks = document.querySelectorAll("#phone-nav .nav-links")
+let menuOpen = false;
+
+
+
+console.log(navLinks)
+
+    navLinks.forEach(link => {
+        link.addEventListener("click", (j) => {
+            
+            console.log("link clicked")
+            body.style.overflow = "unset"
+
+            menuBtn.classList.remove('open');
+            menuOpen = false;
+    
+            navBar.style.width = "0px"
+            setTimeout(() => {
+                navBar.style.display = "none"
+            }, 100)
+        })
+    })
+
 
 menuBtn.addEventListener("click", function()
 {
 
-    if(navBar.style.display === "block")
+    console.log(navBar.style.display)
+    console.log(navBar.style.width)
+    console.log(navBar)
+
+    if(navBar.style.width === "100%")
     {
-        return navBar.style.display = "none";
+
+        body.style.overflow = "unset"
+
+        menuBtn.classList.remove('open');
+        menuOpen = false;
+
+        navBar.style.width = "0px"
+        setTimeout(() => {
+            navBar.style.display = "none"
+        }, 100)
     }
 
     else
     {
-        return navBar.style.display = "block";
+        body.style.overflow = "hidden"
+
+
+        menuBtn.classList.add('open');
+        menuOpen = true;
+
+        navBar.style.display = "block"
+        
+        setTimeout(() => {
+            navBar.style.width = "100%"
+        }, 10)
+
     }
 })
